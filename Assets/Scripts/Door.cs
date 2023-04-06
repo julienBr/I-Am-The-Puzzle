c0c7f258ce25b44843e4212e15a0cb957b1f3e47
julienBr <julien.bringard@outlook.fr>
2023-04-06 16:58:27 +0200

    Add HoverState

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private Animator _animator;
+    private bool _isOpen;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void Interact()
+    {
+        if (!_isOpen)
+        {
+            _isOpen = true;
+            _animator.SetBool("OpenDoor", true);
+        }
+        else
+        {
+            _isOpen = false;
+            _animator.SetBool("OpenDoor", false);
+        }
+    }
+}
