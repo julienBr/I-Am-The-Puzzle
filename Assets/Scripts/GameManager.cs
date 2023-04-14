6ffa93453473f97d0b39797e06c72e001ca7ed13
julienBr <julien.bringard@outlook.fr>
2023-04-14 14:57:00 +0200

    Update Scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,16 +2,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    //[SerializeField] private AppDatas choice;
     [SerializeField] private GameObject fade;
-    [SerializeField] private GameObject _mainMenu;
-    [SerializeField] private GameObject _difficultyMenu;
-
-    public void NewGame()
-    {
-        _mainMenu.SetActive(false);
-        _difficultyMenu.SetActive(true);
-    }
 
     public void Quit () 
     {
