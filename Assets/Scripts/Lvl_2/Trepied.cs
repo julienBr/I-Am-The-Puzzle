de26f65c781d782e60b261f20a61cb972b7b3b3f
julienBr <julien.bringard@outlook.fr>
2023-04-28 13:44:38 +0200

    Update Packages

--- Assets/Scripts/Lvl_2/Trepied.cs
+++ Assets/Scripts/Lvl_2/Trepied.cs
@@ -1,26 +1,6 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Trepied : MonoBehaviour
 {
-    public Transform _posTrepied;
-    public List<Transform> _posTarget;
-    public bool hit;
-    private void Update()
-    {
-        hit = CheckTarget();
-        Debug.DrawRay(_posTrepied.position, _posTrepied.forward*10f, Color.green);
-    }
-
-    private bool CheckTarget()
-    {
-        foreach (Transform target in _posTarget)
-        {
-            if (Physics.Raycast(_posTrepied.position, _posTrepied.forward, out RaycastHit hit))
-            {
-                if (hit.collider.gameObject.CompareTag("targetLaser")) return true;
-            }
-        }
-        return false;
-    }
+    
 }
