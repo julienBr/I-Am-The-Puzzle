f96bb3b5996e60c14215c6149fc089c19b106b3c
julienBr <julien.bringard@outlook.fr>
2023-04-28 17:03:12 +0200

    Update Scene2

--- Assets/Scripts/Lvl_2/LaserManager.cs
+++ Assets/Scripts/Lvl_2/LaserManager.cs
@@ -9,25 +9,28 @@ public class LaserManager : MonoBehaviour
     public List<Transform> _posTarget1;
     public List<Transform> _posTarget2;
     public List<Transform> _posTarget3;
+    public List<LineRenderer> _laser1;
+    public List<LineRenderer> _laser2;
+    public List<LineRenderer> _laser3;
     public List<bool> hit1;
     public List<bool> hit2;
     public List<bool> hit3;
 
     private void Update()
     {
-        foreach (Transform transform in _trepied1)
+        /*foreach (Transform transform in _trepied1)
             Debug.DrawRay(transform.position, transform.forward * 20f, Color.green);
         foreach (Transform transform in _trepied2)
             Debug.DrawRay(transform.position, transform.forward * 20f, Color.red);
         foreach (Transform transform in _trepied3)
-            Debug.DrawRay(transform.position, transform.forward * 20f, Color.blue);
+            Debug.DrawRay(transform.position, transform.forward * 20f, Color.blue);*/
         RotateToTarget();
         CheckHitTarget();
     }
 
     private void RotateToTarget()
     {
-        for(int i=0; i<_trepied1.Count; i++)
+        for(int i=0; i<4; i++)
         {
             _trepied1[i].LookAt(_posTarget1[i]);
             _trepied2[i].LookAt(_posTarget2[i]);
@@ -37,20 +40,56 @@ public class LaserManager : MonoBehaviour
 
     private void CheckHitTarget()
     {
-        for (int i=0; i<_trepied1.Count; i++)
+        for (int i=0; i<4; i++)
         {
             if(Physics.Raycast(_trepied1[i].position, _trepied1[i].forward, out RaycastHit rayHit1))
-                if (rayHit1.collider.gameObject.CompareTag("targetLaser")) 
+            {
+                if (rayHit1.collider.gameObject.CompareTag("Target"))
+                {
+                    //Debug.DrawRay(_trepied1[i].position, _trepied1[i].forward*20f, Color.green);
                     hit1[i] = true;
-                else hit1[i] = false;
-            if(Physics.Raycast(_trepied2[i].position, _trepied2[i].forward, out RaycastHit rayHit2))
-                if (rayHit2.collider.gameObject.CompareTag("targetLaser")) 
+                    _laser1[i].enabled = true;
+                    _laser1[i].SetPosition(0, _trepied1[i].position);
+                    _laser1[i].SetPosition(1, _posTarget1[i].position);
+                }
+                else
+                {
+                    _laser1[i].enabled = false;
+                    hit1[i] = false;
+                }
+            }
+            /*if(Physics.Raycast(_trepied2[i].position, _trepied2[i].forward, out RaycastHit rayHit2))
+            {
+                if (rayHit2.collider.gameObject.CompareTag("targetLaser"))
+                {
+                    //Debug.DrawRay(_trepied2[i].position, _trepied2[i].forward*20f, Color.red);
                     hit2[i] = true;
-                else hit2[i] = false;
-            if(Physics.Raycast(_trepied3[i].position, _trepied3[i].forward, out RaycastHit rayHit3))
-                if (rayHit3.collider.gameObject.CompareTag("targetLaser")) 
+                    _laser2[i].enabled = true;
+                    _laser2[i].SetPosition(0, _trepied2[i].position);
+                    _laser2[i].SetPosition(1, _posTarget2[i].position);
+                }
+                else
+                {
+                    hit2[i] = false;
+                    _laser2[i].enabled = false;
+                }
+            }*/
+            /*if(Physics.Raycast(_trepied3[i].position, _trepied3[i].forward, out RaycastHit rayHit3))
+            {
+                if (rayHit3.collider.gameObject.CompareTag("targetLaser"))
+                {
+                    //Debug.DrawRay(_trepied3[i].position, _trepied3[i].forward * 20f, Color.blue);
                     hit3[i] = true;
-                else hit3[i] = false;
+                    _laser3[i].enabled = true;
+                    _laser3[i].SetPosition(0, _trepied3[i].position);
+                    _laser3[i].SetPosition(1, _posTarget3[i].position);
+                }
+                else
+                {
+                    hit3[i] = false;
+                    _laser2[i].enabled = false;
+                }
+            }*/
         }
     }
 }
