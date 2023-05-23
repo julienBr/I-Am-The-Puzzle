05de23a86b06187d90115d7e8fe898ae670ff297
Michael-elie <120110492+Michael-elie@users.noreply.github.com>
2023-05-23 16:57:57 +0200

    mirror dev

--- Assets/Scripts/Lvl_1/FollowMiror.cs
+++ Assets/Scripts/Lvl_1/FollowMiror.cs
@@ -6,8 +6,12 @@ public class FollowMiror : MonoBehaviour
 {
     public Transform _objectToFollow;
     public Transform _mirror;
-    private bool _raycastHitObstacle = false;
-   
+    /* private bool _raycastHitObstacle = false;
+     private bool _raycastHitObstacle2 = false;
+     [SerializeField] private GameObject _obstacle1Zone;
+     [SerializeField] private GameObject _obstacle2Zone;*/
+    [SerializeField] private Vector3 initialPosition;
+    [SerializeField] private Vector3 finalPosition;
     
     void Start()
     {
@@ -16,18 +20,23 @@ public class FollowMiror : MonoBehaviour
     
     public void FollowPlayer()
     {
-        if (_raycastHitObstacle == false) // Vérification raycast object
+      /*  if (_raycastHitObstacle == false && _raycastHitObstacle2 == false ) // Vérification raycast object
         {
            
-            Vector3 objectToFollowLocal = _mirror.InverseTransformPoint(_objectToFollow.position);
-            transform.position = _mirror.TransformPoint(new Vector3(objectToFollowLocal.x, objectToFollowLocal.y, -objectToFollowLocal.z));
-           
-        }
-        else 
-        {
             
         }
+        else  if (_raycastHitObstacle == true)
+        {
+            transform.position = _obstacle1Zone.transform.position;
+        }
+        else if (_raycastHitObstacle2 == true)
+        {
+            transform.position = _obstacle2Zone.transform.position;
+        }*/
         
+      Vector3 objectToFollowLocal = _mirror.InverseTransformPoint(_objectToFollow.position);
+      initialPosition = transform.position;
+      transform.position = _mirror.TransformPoint(new Vector3(objectToFollowLocal.x, objectToFollowLocal.y, -objectToFollowLocal.z)) ;
 
 
         
@@ -35,23 +44,44 @@ public class FollowMiror : MonoBehaviour
 
     private void Update()
     {
-        
-        transform.rotation = _objectToFollow.rotation;
-        //Vector3 objectToFollowLocal = _mirror.InverseTransformPoint(_objectToFollow.position);
-        //transform.position = _mirror.TransformPoint(new Vector3(objectToFollowLocal.x, objectToFollowLocal.y, -objectToFollowLocal.z));
+       
+        transform.localRotation = _objectToFollow.localRotation;
 
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 10f, Color.yellow);
-            Debug.Log("Did Hit" + hit.collider.gameObject.name);
-
-
-            if (hit.collider.gameObject.tag == "obstacle")
-            {
-                Debug.Log("LETSGOOOOO");
-            }
         }
+        
+
+
+        /*RaycastHit hit;
+         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+         {
+             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 10f, Color.yellow);
+            
+ 
+ 
+             if (hit.collider.gameObject.name == "Obstacle1")
+             {
+                 _raycastHitObstacle = true;
+                 Debug.Log("Did Hit" + hit.collider.gameObject.name);
+             }
+             else if (hit.collider.gameObject.name == "Obstacle2")
+             {
+                 _raycastHitObstacle2 = true;
+                 Debug.Log("Did Hit" + hit.collider.gameObject.name);
+             }
+             else
+             {
+                 _raycastHitObstacle = false;
+                 _raycastHitObstacle2 = false;
+             }
+         }*/
+
+
+    }
 
     }
 
@@ -59,5 +89,5 @@ public class FollowMiror : MonoBehaviour
 
 
 
-}
+
 
---
+++ Assets/Scripts/Lvl_1/ObjectFollowMiror.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFollowMiror : MonoBehaviour
+{
+    public Transform _objectToFollow;
+    public Transform _mirror;
+    
+    
+
+    private void Update()
+    {
+        
+        
+        Vector3 objectToFollowLocal = _mirror.InverseTransformPoint(_objectToFollow.position);
+        transform.position = _mirror.TransformPoint(new Vector3(objectToFollowLocal.x, objectToFollowLocal.y, -objectToFollowLocal.z));
+        transform.localRotation = _objectToFollow.localRotation;
+
+      
+
+    }
+
+
+
+
+
+}
+
