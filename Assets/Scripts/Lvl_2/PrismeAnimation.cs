e75827d652e31d8d958b2b97912e1b3b642312c7
Michael-elie <120110492+Michael-elie@users.noreply.github.com>
2023-05-26 16:33:45 +0200

    LVl2

---
+++ Assets/Scripts/Lvl_2/PrismeAnimation.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PrismeAnimation : MonoBehaviour
+{
+    public Color redColor;
+    public Color blueColor;
+    public Color originalColor;
+    public float RotationSpeed = 2f;
+    public bool _laserActivated = false;
+   
+    void Start()
+    {
+        //GetComponent<MeshRenderer>().material.SetColor("Color_0a8bc23ce2644fbf9e887b1975e13b04", customColor);
+        
+    }
+
+    private void Update()
+    {
+        
+        if (_laserActivated == true)
+        {
+            transform.Rotate(0f,RotationSpeed,0f);
+            gameObject.GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", redColor);
+        }
+
+        
+    }
+
+    public void PrismeActivated()
+    {
+        _laserActivated = true;
+    }
+}
