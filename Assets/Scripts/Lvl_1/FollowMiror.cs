cac624e9de301077717a17a67390318aacd771b8
Michael-elie <120110492+Michael-elie@users.noreply.github.com>
2023-09-26 14:39:58 +0200

    UPDATE LVL1

--- Assets/Scripts/Lvl_1/FollowMiror.cs
+++ Assets/Scripts/Lvl_1/FollowMiror.cs
@@ -7,45 +7,20 @@ public class FollowMiror : MonoBehaviour
     public Transform _objectToFollow;
 
     public Transform _mirror;
-
-    /* private bool _raycastHitObstacle = false;
-     private bool _raycastHitObstacle2 = false;
-     [SerializeField] private GameObject _obstacle1Zone;
-     [SerializeField] private GameObject _obstacle2Zone;*/
-    [SerializeField] private Vector3 initialPosition;
-    [SerializeField] private Vector3 finalPosition;
-    [SerializeField] private Vector3 direction;
-
-    void Start()
-    {
-
-    }
-
-    public void FollowPlayer()
-    {
-        
-    }
+    
+    
 
     private void Update()
     {
         Vector3 objectToFollowLocal = _mirror.InverseTransformPoint(_objectToFollow.position);
-        initialPosition = transform.position;
-        finalPosition = new Vector3(objectToFollowLocal.x, objectToFollowLocal.y, -objectToFollowLocal.z);
-        transform.position = _mirror.TransformPoint(finalPosition);
-        direction = finalPosition - initialPosition;
 
-        RaycastHit hit;
-        if (Physics.Raycast(initialPosition, direction, out hit, Mathf.Infinity))
+        transform.position =
+            _mirror.TransformPoint(new Vector3(objectToFollowLocal.x, objectToFollowLocal.y, -objectToFollowLocal.z));
 
-        {
-            Debug.DrawRay(initialPosition, direction * 100f, Color.yellow);
+        transform.localRotation = _objectToFollow.localRotation;
 
-            if (hit.collider.gameObject.name == "Obstacle1")
-            {
-                Debug.Log("OBSTACLE TOUCHERRRRR");
-            }
-            
-        }
+
+
     }
 
 }
